ce8045bcb285ca4169c54c57c08b35a7684a3ce0
Andrew <withapoll@outlook.com>
2024-12-29 01:35:40 +0300

    added cname

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "copy-cname",
+      writeBundle() {
+        fs.copyFileSync("CNAME", "dist/CNAME");
+      },
+    },
+  ],
   base: "https://withapoll.com/",
 });
